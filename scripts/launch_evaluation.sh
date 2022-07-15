fd1ce1bc2fbbd149e0d08640c22f6dc783e3b2cd
Michael Santoro <michael.b.santoro@gmail.com>
2022-07-15 22:08:16 +0000

    adding script files

---
+++ scripts/launch_evaluation.sh
@@ -0,0 +1,4 @@
+python experiments/model_main_tf2.py \
+--model_dir=/home/workspace/experiments/reference/ \
+--pipeline_config_path=/home/workspace/UProj1/experiments/experiment0/pipeline_new.config \
+--checkpoint_dir=/home/workspace/experiments/reference/
