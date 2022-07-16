565ae1e46733f217ef2f940dd859a697476426b5
Michael Santoro <michael.b.santoro@gmail.com>
2022-07-16 17:31:04 +0000

    add config for new experiment

---
+++ experiments/experiment4/start_training.sh
@@ -0,0 +1,3 @@
+python experiments/model_main_tf2.py \
+--model_dir=/home/workspace/experiments/experiment4 \
+--pipeline_config_path=/home/workspace/UProj1/experiments/experiment4/pipeline_new.config
