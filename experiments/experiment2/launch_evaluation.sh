4e72065d8dd7930bc3cd6ccc47c69b966920989a
Michael Santoro <michael.b.santoro@gmail.com>
2022-07-16 15:13:33 +0000

    added evaluation file to experiment folder

---
+++ experiments/experiment2/launch_evaluation.sh
@@ -0,0 +1,4 @@
+python experiments/model_main_tf2.py \
+--model_dir=/home/workspace/experiments/experiment2/ \
+--pipeline_config_path=/home/workspace/UProj1/experiments/experiment2/pipeline_new.config \
+--checkpoint_dir=/home/workspace/experiments/experiment2/
