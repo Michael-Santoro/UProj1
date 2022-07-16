40fb1a72e6ecc9fd7f72378a134622da06dc9982
Michael Santoro <michael.b.santoro@gmail.com>
2022-07-16 15:30:27 +0000

    modifying the experiment 3

---
+++ experiments/experiment3/start_training.sh
@@ -0,0 +1,3 @@
+python experiments/model_main_tf2.py \
+--model_dir=/home/workspace/experiments/experiment3 \
+--pipeline_config_path=/home/workspace/UProj1/experiments/experiment3/pipeline_new.config
